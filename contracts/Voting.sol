a83f632cdcc3eb59604b0111f148ac1eb090052a
KimchiChamchi <esm2103@gmail.com>
2022-02-19 23:56:25 +0900

    득표수 매핑 대신 구조체에 기록하도록 변경

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -24,14 +24,10 @@ contract Voting {
 
     // 후보자 주소를 통해 후보자 구조체에 매핑
     mapping(address => Candidate) public asdf;
-    // 투표하면 후보자 주소를 통해 득표하도록 매핑
-    mapping(address => uint256) public poll;
     // 투표하면 내 주소를 통해 투표권 사라지도록 매핑
     mapping(address => bool) public suffrage;
     // 한 사용자가 후보자 중복등록을 방지하기 위한 매핑
     mapping(address => bool) public candidateDuplication;
-    // 후보자번호에 후보자 주소 저장을 위한 매핑
-    mapping(uint8 => address) public numToCandidate;
 
     // 후보자 등록 함수
     function candidateRegistration(uint8 _age, string memory _name)
@@ -48,8 +44,6 @@ contract Voting {
         // require(msg.value >= registrationFee);
         // 후보자가 입력한 정보로 구조체 생성
         CandidateList.push(Candidate(candidateNum, _age, _name, 0));
-        // 후보자번호에 후보자 주소 저장
-        numToCandidate[candidateNum] = msg.sender;
         // 다음 후보자 번호 다음번호로 바꿔주기
         candidateNum++;
         // 같은 사용자가 후보등록을 또 할 수 없도록 제한
@@ -63,17 +57,15 @@ contract Voting {
         // 투표를 이미 한 사람은 투표권을 행사했으니 멈춰!
         require(suffrage[msg.sender] == false, "you have already voted");
 
-        address voteBox = numToCandidate[_number];
-        // 후보에게 투표하면 해당 후보 주소로 매핑해서 1표 추가
-        poll[voteBox] = poll[voteBox] + 1;
+        // 해당 후보구조체에 1표 추가
+        CandidateList[_number - 1].poll++;
         // 투표했으면 내 투표권 없애기
         suffrage[msg.sender] = true;
         // 내 투표로 인해 해당 후보자가 10표째 득표라면
-        require(1 == 1, "qqqqqqqqqqqqqqqqqq");
-        if (poll[voteBox] == 10) {
-            // 당선함수 실행
-            // AnnouncementOfResults(number, name, age);
-        }
+        // if (CandidateList[_number - 1] == 5) {
+        //     // 당선함수 실행
+        //     // AnnouncementOfResults(number, name, age);
+        // }
     }
 
     // 축 당선 발표 함수
