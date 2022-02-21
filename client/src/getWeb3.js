60ad2360b2bfbb44f68d1a9cf80f63b026a43f97
KimchiChamchi <esm2103@gmail.com>
2022-02-21 10:52:19 +0900

    웹3 주석 한글번역

--- client/src/getWeb3.js
+++ client/src/getWeb3.js
@@ -2,34 +2,34 @@ import Web3 from "web3";
 
 const getWeb3 = () =>
   new Promise((resolve, reject) => {
-    // Wait for loading completion to avoid race conditions with web3 injection timing.
+    // web3 주입 타이밍으로 경쟁 조건을 피하기 위해 로드 완료를 기다립니다.
     window.addEventListener("load", async () => {
-      // Modern dapp browsers...
+      // 최신 dapp 브라우저 ...
       if (window.ethereum) {
         const web3 = new Web3(window.ethereum);
         try {
-          // Request account access if needed
+          // 필요한 경우 계정 액세스 요청
           await window.ethereum.enable();
-          // Accounts now exposed
+          // 현재 노출된 계정
           resolve(web3);
         } catch (error) {
           reject(error);
         }
       }
-      // Legacy dapp browsers...
+      // 기존 dapp 브라우저...
       else if (window.web3) {
-        // Use Mist/MetaMask's provider.
+        // Mist/MetaMask의 공급자를 사용하십시오.
         const web3 = window.web3;
-        console.log("Injected web3 detected.");
+        console.log("주입된 web3가 감지되었습니다.");
         resolve(web3);
       }
-      // Fallback to localhost; use dev console port by default...
+      // 로컬 호스트로 대체; 기본적으로 dev 콘솔 포트를 사용...
       else {
         const provider = new Web3.providers.HttpProvider(
           "http://127.0.0.1:8545"
         );
         const web3 = new Web3(provider);
-        console.log("No web3 instance injected, using Local web3.");
+        console.log("Local web3를 사용하여 주입된 web3 인스턴스가 없습니다.");
         resolve(web3);
       }
     });
