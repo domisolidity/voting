a1b5c0b0df7f937c5f747bd13f0349477d110a8f
pier101 <85658044+pier101@users.noreply.github.com>
2022-02-21 11:08:16 +0900

    1_setting

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     try {
       // Get network provider and web3 instance.
       const web3 = await getWeb3();
-   
+   console.log("dd")
 
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
