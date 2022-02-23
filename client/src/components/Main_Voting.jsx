24ab4f2d8058d87a4c4f4993418c96bf556e7bc0
KimchiChamchi <esm2103@gmail.com>
2022-02-23 22:32:53 +0900

    투표하면 페이지에 득표수 갱신되도록 수정

--- client/src/components/Main_Voting.jsx
+++ client/src/components/Main_Voting.jsx
@@ -2,43 +2,48 @@ import React, { useState, useRef, useEffect } from "react";
 import "../styles/Candidate.css";
 
 const Main_Voting = (props) => {
-  const {web3, voteContract, accounts, isLoading} = props.initialization;
+  const { web3, voteContract, accounts, isLoading } = props.initialization;
 
   const [allCandidate, setAllCandidate] = useState(0);
   const [receivedVote, setReceivedVote] = useState(0);
 
+  useEffect(() => {
+    console.log("유즈이펙트3");
+    if (voteContract == undefined) return;
+  }, [voteContract, props.step]);
 
-  
-  useEffect(() => {  
-    console.log("유즈이펙트3")
-    if(voteContract == undefined) return
+  const getAllCandidates = async () => {
+    await voteContract.methods
+      .getCandidate()
+      .call()
+      .then((result) => setAllCandidate(result));
+  };
 
-    const getAllCandidates = async() =>{
-      await voteContract.methods.getCandidate().call()
-      .then(result=> setAllCandidate(result));
-    }
-    getAllCandidates();
-  }, [voteContract,props.step])
-
-
-
-  const submitVote = async(e) =>{
-    console.log("투표하기 e.target.value : ", e.target.value)
-    await voteContract.methods.vote(e.target.value).send({from: accounts[0]})
-      .then(voteResult => console.log(voteResult))
-      .catch(err=>{
+  const submitVote = async (e) => {
+    console.log("투표하기 e.target.value : ", e.target.value);
+    await voteContract.methods
+      .vote(e.target.value)
+      .send({ from: accounts[0] })
+      .then((voteResult) => {
+        console.log(voteResult);
+        getAllCandidates();
+      })
+      .catch((err) => {
         alert("이미 투표하셨습니다.");
         console.error(err);
-       })
-      
-    await voteContract.methods.getCandidateReceivedVote(e.target.value).call()
-    .then(result=> { setReceivedVote(result)});
+      });
 
-  }
+    await voteContract.methods
+      .getCandidateReceivedVote(e.target.value)
+      .call()
+      .then((result) => {
+        setReceivedVote(result);
+      });
+  };
 
   return (
     <>
-        <div>
+      <div>
         {/* <button type="button" onClick={test}>확인용</button> */}
         <hr className="boundary" />
         <div className="juseok">
@@ -47,33 +52,37 @@ const Main_Voting = (props) => {
         <div className="vote_box">
           <div className="vote_box-title">후보자 명단</div>
           <div className="candidate_list">
-            {allCandidate && allCandidate.map((candidate)=>{
-              return (
-                <>
-                <div className="candidate_list-box">
-                  <div className="photo_box">
-                    <img src={require('../images/'+ candidate.candidateNum + '.png')} />
-                  </div>
-                  <div>{candidate.name}</div>
-                  <div>{candidate.age}</div>
-                  <div>{candidate.receivedVote}</div>
-                  <button
-                    value={candidate.candidateNum}
-                    className="candidate_list-box-button"
-                    onClick={submitVote}
-                  >
-                    투표하기
-                  </button>
-                </div>
-                
-                </>
-              ) 
-            })}
+            {allCandidate &&
+              allCandidate.map((candidate) => {
+                return (
+                  <>
+                    <div className="candidate_list-box">
+                      <div className="photo_box">
+                        <img
+                          src={require("../images/" +
+                            candidate.candidateNum +
+                            ".png")}
+                        />
+                      </div>
+                      <div>{candidate.name}</div>
+                      <div>{candidate.age}</div>
+                      <div>{candidate.receivedVote}</div>
+                      <button
+                        value={candidate.candidateNum}
+                        className="candidate_list-box-button"
+                        onClick={submitVote}
+                      >
+                        투표하기
+                      </button>
+                    </div>
+                  </>
+                );
+              })}
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Main_Voting
+export default Main_Voting;
