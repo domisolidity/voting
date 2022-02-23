49df917a257bd85879295fb35386b4325833d730
codecoco <isawmy@nate.com>
2022-02-23 17:20:27 +0900

    타이틀에 메인링크연결 및 컴포넌트 최상위 조정

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 
-import React, {  useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import VoteContract from "./contracts/Vote.json";
 import getWeb3 from "./getWeb3";
@@ -9,13 +9,15 @@ import Vote from "./pages/VotePage";
 import Register from "./pages/RegisterPage";
 import Step from './components/Step';
 
+import Title from "./components/Title";
+
 const App = () => {
   const [web3, setWeb3] = useState()
   const [accounts, setAccounts] = useState()
   const [voteContract, setVoteContract] = useState()
   const [isLoading, setIsLoading] = useState(false)
-  
-  const init = async () =>{
+
+  const init = async () => {
     setIsLoading(true);
     try {
       console.log("유즈이펙트1")
@@ -29,35 +31,35 @@ const App = () => {
       const voteContract = await new web3.eth.Contract(
         VoteContract.abi,
         deployedNetwork && deployedNetwork.address,
-        );
-        
-        setWeb3(web3);
-        setAccounts(accounts);
-        setVoteContract(voteContract);
-        setIsLoading(false);
-        console.log("유즈이펙트");
-      } catch (error) {
-        alert(`Failed to load web3, accounts, or contract. Check console for details.`);
-        console.error(error);
-      }
-    }
-    
-    useEffect(async() => {
-        init();      
-      }, [])
-      
-    const initialization = {web3,accounts,voteContract,isLoading}
-      
-    return (
-      <BrowserRouter>
+      );
 
-        <Routes>
-          {console.log("페이지")}
-            <Route path="/" exact element={<Vote initialization={initialization}/>}></Route>
-            <Route path="/register" exact element={<Register initialization={initialization}/>}></Route>
-        </Routes>
-      </BrowserRouter>
-    );
+      setWeb3(web3);
+      setAccounts(accounts);
+      setVoteContract(voteContract);
+      setIsLoading(false);
+      console.log("유즈이펙트");
+    } catch (error) {
+      alert(`Failed to load web3, accounts, or contract. Check console for details.`);
+      console.error(error);
+    }
+  }
+
+  useEffect(async () => {
+    init();
+  }, [])
+
+  const initialization = { web3, accounts, voteContract, isLoading }
+
+  return (
+    <BrowserRouter>
+      <Title />
+      <Routes>
+        {console.log("페이지")}
+        <Route path="/" exact element={<Vote initialization={initialization} />}></Route>
+        <Route path="/register" exact element={<Register initialization={initialization} />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
--- client/src/components/Title.jsx
+++ client/src/components/Title.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import "../styles/Title.css";
+import { Link } from "react-router-dom";
 
 function Title() {
   return (
-    <div className="team_name">
-      <div className="do">도</div>
-      <div className="dot">·</div>
-      <div className="re">레</div>
-      <div className="dot">·</div>
-      <div className="mi">미</div>
-      <div className="dot">·</div>
-      <div className="fa">파</div>
-      <div className="dot">·</div>
-      <div className="so">솔</div>
-      <div className="dot">·</div>
-      <div className="li">리</div>
-      <div className="dot">·</div>
-      <div className="di">디</div>
-      <div className="dot">·</div>
-      <div className="ty">티</div>
+    <div>
+      <Link className="team_name" to="/">
+        <div className="do">도</div>
+        <div className="dot">·</div>
+        <div className="re">레</div>
+        <div className="dot">·</div>
+        <div className="mi">미</div>
+        <div className="dot">·</div>
+        <div className="fa">파</div>
+        <div className="dot">·</div>
+        <div className="so">솔</div>
+        <div className="dot">·</div>
+        <div className="li">리</div>
+        <div className="dot">·</div>
+        <div className="di">디</div>
+        <div className="dot">·</div>
+        <div className="ty">티</div>
+      </Link>
     </div>
   );
 }
